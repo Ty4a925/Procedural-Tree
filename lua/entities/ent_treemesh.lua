1088c11f81db6758833c7dd294145326d0ecf02b
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2025-03-24 16:12:41 +0300

    Linter pass

--- lua/entities/ent_treemesh.lua
+++ lua/entities/ent_treemesh.lua
@@ -17,53 +17,50 @@ function ENT:SetupDataTables()
 end
 
 function ENT:Initialize()
-    if SERVER then
-        self:SetModel("models/hunter/blocks/cube025x025x025.mdl")
-        self:PhysicsInit(SOLID_BBOX)
+	if SERVER then
+		self:SetModel("models/hunter/blocks/cube025x025x025.mdl")
+		self:PhysicsInit(SOLID_BBOX)
 
-        local phys = self:GetPhysicsObject()
+		local phys = self:GetPhysicsObject()
 
-        if phys:IsValid() then
-            phys:EnableMotion(false)
-        end
-    else
-        local size = self:GetTreeSize()
-        local huhu = 10 * size
+		if phys:IsValid() then
+			phys:EnableMotion(false)
+		end
+	else
+		local size = self:GetTreeSize()
+		local huhu = 10 * size
 
-        local obbmax, obbmin = Vector(huhu, huhu, huhu * 2.5), Vector(huhu, huhu, 0)
-        self:SetRenderBounds(-obbmin, obbmax)
+		local obbmax, obbmin = Vector(huhu, huhu, huhu * 2.5), Vector(huhu, huhu, 0)
+		self:SetRenderBounds(-obbmin, obbmax)
 
-        self.TreeSticks = {}
-        self.TreeUP = 1
+		self.TreeSticks = {}
+		self.TreeUP = 1
 
-        local function StickGEN(pos, pos2, gen, len, rad, num)
+		local function StickGEN(pos, pos2, gen, len, rad, num)
+			if gen <= 0 then return end
 
-            if gen <= 0 then return end
+			local stick = {
+				StartPos = pos,
+				Length = len,
+				Growth = 0,
+				PosTo = pos2,
+				Radius = rad,
+				Gen = num
+			}
 
-            local stick = {
-                StartPos = pos,
-                --EndPos = pos + pos2 * len,
-                Length = len,
-                Growth = 0,
-                PosTo = pos2,
-                Radius = rad,
-                Gen = num
-            }
+			self.TreeSticks[#self.TreeSticks + 1] = stick
 
-            self.TreeSticks[#self.TreeSticks + 1] = stick
+			if num < 6 then
+				local rand = math.random(2, 4)
+				for i = 1, rand do
+					local pos3 = (pos2 + VectorRand() * 0.7):GetNormalized()
+					StickGEN(pos + pos2 * len, pos3, gen - 1, len * 0.7, rad * 0.7, num + 1)
+				end
+			end
+		end
 
-            if num < 6 then
-                local rand = math.random(2, 4)
-                for i = 1, rand do
-                    local pos3 = (pos2 + VectorRand() * 0.7):GetNormalized()
-                    StickGEN(pos + pos2 * len, pos3, gen - 1, len * 0.7, rad * 0.7, num + 1)
-                end
-            end
-
-        end
-        
-        StickGEN(vector_origin, Vector(0, 0, 1), self.TreeHD, 16.5 / 2 * size, 1 / 2 * size, 1)
-    end
+		StickGEN(vector_origin, Vector(0, 0, 1), self.TreeHD, 16.5 / 2 * size, 1 / 2 * size, 1)
+	end
 end
 
 if SERVER then return end
@@ -71,90 +68,86 @@ if SERVER then return end
 local mat = Material("models/props_foliage/tree_deciduous_01a_trunk")
 
 function ENT:OnRemove()
-    if IsValid(self.RENDER_MESH) then
-        self.RENDER_MESH:Destroy()
-    end
+	if IsValid(self.RENDER_MESH) then
+		self.RENDER_MESH:Destroy()
+	end
 end
 
 function ENT:GetRenderMesh()
-    local MESH = {}
+	local MESH = {}
 
-    local upping = true
+	local upping = true
 
-    if self.TreeSticks then
-        for i=1, #self.TreeSticks do
-            local stick = self.TreeSticks[i]
-            if stick.Gen == self.TreeUP then
-                local lerp = stick.Growth
-                stick.Growth = Lerp(FrameTime() * 45, lerp, 1)
+	if self.TreeSticks then
+		for i = 1, #self.TreeSticks do
+			local stick = self.TreeSticks[i]
 
-                if lerp < 0.999 then upping = false end
-            end
+			if stick.Gen == self.TreeUP then
+				local lerp = stick.Growth
+				stick.Growth = Lerp(FrameTime() * 45, lerp, 1)
 
-            local startpos = stick.StartPos
+				if lerp < 0.999 then upping = false end
+			end
 
-            local grownLength = stick.Length * stick.Growth
-            local endPos = startpos + stick.PosTo * grownLength
+			local startpos = stick.StartPos
 
-            local xy = stick.Radius / 2
+			local grownLength = stick.Length * stick.Growth
+			local endPos = startpos + stick.PosTo * grownLength
 
-            local base1 = startpos + Vector(-xy, -xy, 0)
-            local base2 = startpos + Vector(xy, -xy, 0)
-            local base3 = startpos + Vector(xy, xy, 0)
-            local base4 = startpos + Vector(-xy, xy, 0)
+			local xy = stick.Radius / 2
 
-            local top1 = endPos + Vector(-xy, -xy, 0)
-            local top2 = endPos + Vector(xy, -xy, 0)
-            local top3 = endPos + Vector(xy, xy, 0)
-            local top4 = endPos + Vector(-xy, xy , 0)
+			local base1 = startpos + Vector(-xy, -xy, 0)
+			local base2 = startpos + Vector(xy, -xy, 0)
+			local base3 = startpos + Vector(xy, xy, 0)
+			local base4 = startpos + Vector(-xy, xy, 0)
 
-            local faces = {
-                {base1, base2, top2, top1},
-                {base2, base3, top3, top2},
-                {base3, base4, top4, top3},
-                {base4, base1, top1, top4},
-                --{top1, top2, top3, top4},
-                --{base1, base2, base3, base4}
-            }
+			local top1 = endPos + Vector(-xy, -xy, 0)
+			local top2 = endPos + Vector(xy, -xy, 0)
+			local top3 = endPos + Vector(xy, xy, 0)
+			local top4 = endPos + Vector(-xy, xy , 0)
 
-            for i = 1, #faces do
-                local face = faces[i]
-                local face1 = face[1]
-                local face2 = face[2]
-                local face3 = face[3]
-                local face4 = face[4]
+			local faces = {
+				{base1, base2, top2, top1},
+				{base2, base3, top3, top2},
+				{base3, base4, top4, top3},
+				{base4, base1, top1, top4}
+			}
 
-                local nm1 = (face3 - face1):GetNormalized()
-                local nm2 = (face1 - face3):GetNormalized()
-                MESH[#MESH + 1] = { pos = face1, normal = nm1, u = 0, v = 0 }
-                MESH[#MESH + 1] = { pos = face2, normal = (face3 - face2):GetNormalized(), u = 1, v = 0 }
-                MESH[#MESH + 1] = { pos = face3, normal = nm2, u = 1, v = 1 }
+			for i = 1, #faces do
+				local face = faces[i]
+				local face1 = face[1]
+				local face2 = face[2]
+				local face3 = face[3]
+				local face4 = face[4]
 
-                MESH[#MESH + 1] = { pos = face3, normal = nm2, u = 1, v = 1 }
-                MESH[#MESH + 1] = { pos = face4, normal = (face4 - face1):GetNormalized(), u = 0, v = 1 }
-                MESH[#MESH + 1] = { pos = face1, normal = nm1, u = 0, v = 0 }
-            end
-        end
+				local nm1 = (face3 - face1):GetNormalized()
+				local nm2 = (face1 - face3):GetNormalized()
+				MESH[#MESH + 1] = { pos = face1, normal = nm1, u = 0, v = 0 }
+				MESH[#MESH + 1] = { pos = face2, normal = (face3 - face2):GetNormalized(), u = 1, v = 0 }
+				MESH[#MESH + 1] = { pos = face3, normal = nm2, u = 1, v = 1 }
+				MESH[#MESH + 1] = { pos = face3, normal = nm2, u = 1, v = 1 }
+				MESH[#MESH + 1] = { pos = face4, normal = (face4 - face1):GetNormalized(), u = 0, v = 1 }
+				MESH[#MESH + 1] = { pos = face1, normal = nm1, u = 0, v = 0 }
+			end
+		end
 
-        if upping then
-            if self.TreeUP > 6 then
+		if upping then
+			if self.TreeUP > 6 then
+				if IsValid(self.RENDER_MESH) then
+					self.RENDER_MESH:Destroy()
+					self.RENDER_MESH = nil
+				end
 
-                if IsValid(self.RENDER_MESH) then
-                    self.RENDER_MESH:Destroy()
-                    self.RENDER_MESH = nil
-                end
+				self.TreeSticks = nil
+			end
 
-                self.TreeSticks = nil
-            end
-            
-            self.TreeUP = self.TreeUP + 1
-        end
+			self.TreeUP = self.TreeUP + 1
+		end
 
-        
-        self.RENDER_MESH = Mesh()
-        self.RENDER_MESH:BuildFromTriangles(MESH)
-    end
+		self.RENDER_MESH = Mesh()
+		self.RENDER_MESH:BuildFromTriangles(MESH)
+	end
 
-    if !self.RENDER_MESH then return end
-    return { Mesh = self.RENDER_MESH, Material = mat }
+	if not self.RENDER_MESH then return end
+	return { Mesh = self.RENDER_MESH, Material = mat }
 end
